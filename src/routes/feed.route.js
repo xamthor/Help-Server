2a9768b5e740a668b2f68d24e8115a9c56d1473d
Smiley <jcsmileyjr@gmail.com>
2021-01-10 00:34:32 -0600

    Update CORS for feed route

--- src/routes/feed.route.js
+++ src/routes/feed.route.js
@@ -2,12 +2,21 @@ import { Router } from "express";
 import feedController from "../controllers/feed.controller";
 import catchAsync from "../middleware/catchAsync";
 import authentication from "../middleware/authenticate";
+import cors from "cors";
+
+const corsOptions = {
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+}
 
 const { feed } = feedController;
 const { authenticate } = authentication;
 
 const feedRouter = Router();
-
+feedRouter.use(cors(corsOptions));
 feedRouter.get("/all", authenticate, catchAsync(feed));
 
 export default feedRouter;
