7ed031e47f20ac55b4683d16a7d1d26a2e46fdc7
Smiley <jcsmileyjr@gmail.com>
2021-01-15 21:09:00 -0600

    Update the profile route to allow access from the app

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -2,12 +2,20 @@ import { Router } from "express";
 import profileController from "../controllers/profile.controller";
 import catchAsync from "../middleware/catchAsync";
 import authentication from "../middleware/authenticate";
+import cors from "cors";
 
+const corsOptions = {
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+}
 const { update } = profileController;
 const { authenticate } = authentication;
 
 const profileRouter = Router();
-
+profileRouter.use(cors(corsOptions));
 profileRouter.post("/update", authenticate, catchAsync(update));
 
 export default profileRouter;
